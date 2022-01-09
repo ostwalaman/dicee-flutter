7a74c415bdc36d601bf104479dd0114ecada221a
Aman Ostwal <ostwalaman@Amans-MacBook.local>
2022-01-09 18:54:52 +0530

    Random Dicee Generator Flutter

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftSideDice = 1;
+  int rightSideDice = 6;
+  void changeDiceFace() {
+    setState(() {
+      leftSideDice = Random().nextInt(6) + 1;
+      rightSideDice = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$leftSideDice.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$rightSideDice.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
